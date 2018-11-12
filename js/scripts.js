f96a8b1353f3213c3aefebe10e03bc287f3103dd
Mohammed Khalifa <P16183157@LEC-ADMIN.dmu.ac.uk>
2018-11-12 14:42:17 +0000

    completed lab 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const menuToggle  = document.getElementById("menu-toggle");
+menuToggle.addEventListener("click", toggleMenu);
+const menuNav   = document.getElementById("menu-nav");
+
+ function toggleMenu =>  console.log("called toggleMenu")
+
+ 
